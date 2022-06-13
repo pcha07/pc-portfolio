2d36e583148d9d85442e11aa5d22a85d15969889
Prabh Chahal <chahalprabh10@gmail.com>
2022-06-12 17:36:25 -0700

    about text fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,24 +2,32 @@ import React from "react";
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-white text-black'>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-          <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-cyan-400'>
+    <div name="about" className="w-full h-screen bg-white text-black">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-cyan-400">
               About
             </p>
           </div>
           <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>
+              I'm glad you're taking the time to learn about me. Please look
+              around.
+            </p>
           </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p>I'm a web developer with an interest in building quality user experiences.With skills in design, digital marketing, and excellent programming skills, I can be the perfect fit for your project.</p>
-            </div>
-            <div>
-              <p></p>  
-            </div>
+          <div>
+            <p>
+              I'm a web developer with an interest in building quality user
+              experiences.With skills in design, digital marketing, and
+              excellent programming skills, I can be the perfect fit for your
+              project.
+            </p>
           </div>
+        </div>
       </div>
     </div>
   );
